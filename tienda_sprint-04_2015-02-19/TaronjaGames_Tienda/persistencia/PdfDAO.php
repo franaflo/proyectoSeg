70c57f79daa6478e725d0a9745c15b4b594e14fa
Fran <fmnf72@gmail.com>
2015-03-02 13:44:16 +0100

    sprint_04

--- tienda_sprint-04_2015-02-19/TaronjaGames_Tienda/persistencia/PdfDAO.php
+++ tienda_sprint-04_2015-02-19/TaronjaGames_Tienda/persistencia/PdfDAO.php
@@ -7,10 +7,10 @@ function select($idPedido) {
     //REALIZAR CONSULTA
 
     $conexion = getConnection();
-    $consulta = "select articulo.nombreArticulo, detallePedido.idPedido, detallePedido.cantidadArticulo, detallePedido.precioArticulo, usuario.nombreUsuario, usuario.apellido1Usuario, usuario.apellido2Usuario, pedido.fechaPedido 
-                from articulo inner join detallePedido inner join usuario inner join pedido 
-                on articulo.idArticulo=detallePedido.idArticulo and detallePedido.idPedido=pedido.idPedido and pedido.idCliente=usuario.idUsuario 
-                where detallePedido.idPedido=".$idPedido;
+    $consulta = "select articulo.nombreArticulo, detallepedido.idPedido, detallepedido.cantidadArticulo, detallepedido.precioArticulo, usuario.nombreUsuario, usuario.apellido1Usuario, usuario.apellido2Usuario, pedido.fechaPedido 
+                from articulo inner join detallepedido inner join usuario inner join pedido 
+                on articulo.idArticulo=detallepedido.idArticulo and detallepedido.idPedido=pedido.idPedido and pedido.idCliente=usuario.idUsuario 
+                where detallepedido.idPedido=".$idPedido;
 
     //EXTRAER DATOS
 
